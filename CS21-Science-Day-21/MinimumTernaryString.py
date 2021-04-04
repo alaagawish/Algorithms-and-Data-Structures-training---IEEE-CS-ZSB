5d50beda0c2e37c35e7402ae72f68834e016498c
alaagawish <53160167+alaagawish@users.noreply.github.com>
2021-04-05 01:14:44 +0200

    Update MinimumTernaryString.py

--- CS21-Science-Day-21/MinimumTernaryString.py
+++ CS21-Science-Day-21/MinimumTernaryString.py
@@ -2,7 +2,7 @@ strNum=input()
 ones=strNum.count("1")
 # print(ones)
 strNum=strNum.replace("1","")
-# print(k)
+# print(strNum)
 first2=(strNum+'2').find('2')
 # print(first2)
 strNum = strNum[:first2]+ "1"*ones +strNum[first2:]
